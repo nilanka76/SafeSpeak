a31a766b19ef815e8bc51bf9653a3b1560235c53
Nilanka Biswas <52482681+nilanka76@users.noreply.github.com>
2023-10-23 19:13:23 +0530

    basic functions completed

--- main.py
+++ main.py
@@ -1,45 +1,56 @@
-from pvrecorder import PvRecorder
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import speech_recognition as sr
-import wave, struct
-import openai
+
+MODEL_SAVE_PATH = r"E:\GitHub\suicidal-speech-detection\distilbert_base_uncased"
+
+# def save_audio(filename, audio_data):
+#     with open(filename, "wb") as f:
+#         f.write(audio_data)
 
 
 def main():
-    # for index, device in enumerate(PvRecorder.get_available_devices()):
-    #     print(f"[{index}] {device}")
-    # idx = int(input("Enter and audio device: "))
-    #
-    # recorder = PvRecorder(device_index=idx, frame_length=512)  # (32 milliseconds of 16 kHz audio)
-    # audio = []
-    # path = 'audio_recording.wav'
-    #
-    # print("Started")
-    # try:
-    #     recorder.start()
-    #     while True:
-    #         frame = recorder.read()
-    #         audio.extend(frame)
-    # except KeyboardInterrupt:
-    #     recorder.stop()
-    #     with wave.open(path, 'w') as f:
-    #         f.setparams((1, 2, 16000, 512, "NONE", "NONE"))
-    #         f.writeframes(struct.pack("h" * len(audio), *audio))
-    # finally:
-    #     print("Saved")
-    #     recorder.delete()
-
-    # openai.api_key = ""
-    # audio_file = open(r"E:\GitHub\suicidal-speech-detection\audio_recording.wav", "rb")
-    # transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    # print(transcript)
     r = sr.Recognizer()
+    my_text = ""
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source, duration=0.2)
-        print("Listening... (Press Ctrl+C to stop)")
-        audio = r.listen(source)
-        my_text = r.recognize_google(audio)
-        my_text = my_text.lower()
-        print(my_text)
+        try:
+            r.adjust_for_ambient_noise(source, duration=0.3)
+            print("Listening...")
+            audio = r.listen(source, timeout=3)  # after 3 second of silence it will stop recording.
+            my_text = r.recognize_google(audio)
+            print(my_text)
+            # save_audio("recorded_audio.wav", audio.get_wav_data())
+        except sr.WaitTimeoutError:
+            print("Ended...")
+
+    if my_text == "":
+        print("No speech detected")
+        return
+
+    # Load model
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_SAVE_PATH)
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL_SAVE_PATH)
+
+    inputs = tokenizer(my_text, return_tensors="pt", padding=True, truncation=True)
+
+    # Make the prediction
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    predicted_probabilities = torch.sigmoid(outputs.logits)
+
+    predicted_probability_positive_class = predicted_probabilities[0][1].item()
+    predicted_probability_negative_class = predicted_probabilities[0][0].item()
+
+    predicted_label = torch.argmax(outputs.logits, dim=1).item()
+    if predicted_label == 1:
+        predicted_class = "Suicidal"
+        predicted_probability = predicted_probability_positive_class
+    else:
+        predicted_class = "Not Suicidal"
+        predicted_probability = predicted_probability_negative_class
+
+    print(f"Speaker is {predicted_probability*100:.2f}% {predicted_class}")
 
 
 if __name__ == '__main__':
